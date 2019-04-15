60110f97ced77ad4dd6fea59fb15474bd0c7cec9
Harvest Project <admin@harvest-project.org>
2019-04-16 00:05:03 +0200

    Bump shutdown timeout

--- libtorrent_impl/params.py
+++ libtorrent_impl/params.py
@@ -13,7 +13,7 @@ SLOW_LOOP_THRESHOLD = 0.5  # Slower loops than this many seconds emits a warning
 POST_UPDATES_INTERVAL = 3  # Post updates every 3 minutes
 SAVE_RESUME_DATA_INTERVAL = 15 * 60  # Save resume data for all torrents (that need it) every 15 minutes
 UPDATE_SESSION_STATS_INTERVAL = 1  # Interval in seconds for updating session stats
-SHUTDOWN_TIMEOUT = 300  # Seconds to wait for alerts during shutdown to be processed
+SHUTDOWN_TIMEOUT = 600  # Seconds to wait for alerts during shutdown to be processed
 
 ERROR_KEY_LOOP = 'loop'
 ERROR_KEY_ALERT_PROCESSING = 'alert_processing_{}'
